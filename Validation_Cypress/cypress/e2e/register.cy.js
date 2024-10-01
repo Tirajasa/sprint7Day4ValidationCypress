8d652ce28997ff0ad6eb37dad36d4f49e5461eae
Tirajasa <uekineraydin@gmail.com>
2024-10-01 17:06:54 +0300

    Cleaning code extras like  unnecessary command lines.

--- Validation_Cypress/cypress/e2e/register.cy.js
+++ Validation_Cypress/cypress/e2e/register.cy.js
@@ -2,12 +2,12 @@ import { errorMessages } from "../../src/components/Register";
 
 describe('Register Page', () => {
   beforeEach(()=>{
+    //Arrange
     cy.visit('http://localhost:5173');
   })
   describe('Error Messages', () => {
     it('Name input errors for less than 3 chars.', () => {
-      //Arrange
-      //cy.visit('http://localhost:5173');
+    
       //Act
       cy.get('[data-cy="name-input"]').type("ek");
       //Assert
@@ -15,8 +15,7 @@ describe('Register Page', () => {
 
     });
     it('Lastname input errors for less than 3 chars.', () => {
-      //Arrange
-      //cy.visit('http://localhost:5173');
+     
       //Act
       cy.get('[data-cy="lastname-input"]').type("er");
       //Assert
@@ -24,16 +23,14 @@ describe('Register Page', () => {
 
     });
     it('Email input errors if it is not validated such as:uekineraydin@gmail.',()=>{
-        //Arrange
-        //cy.visit('http://localhost:5173');
+     
         //Act
         cy.get('[data-cy="email-input"]').type("uekineraydin@gmail.");
         //Assert
         cy.contains(errorMessages.email);
     });
     it('Password input errors if it is not strong like 1234',()=>{
-      //Arrange
-      //cy.visit('http://localhost:5173');
+     
       //Act
       cy.get('[data-cy="password-input"]').type("1234");
       //Assert
@@ -41,8 +38,7 @@ describe('Register Page', () => {
       cy.get('[data-cy="error-message"]').should("have.text",errorMessages.password);
   }); 
   it('Button is disabled while inputs are invalidated',()=>{
-    //Arrange
-    //cy.visit('http://localhost:5173');
+ 
 
     //Act
     cy.get('[data-cy="name-input"]').type("ek");
@@ -59,8 +55,7 @@ describe('Register Page', () => {
   })
   describe('Inputs are validated', () => {
     it('Button is enabled when all inputs are validated', () => {
-      //Arrange
-      //cy.visit('http://localhost:5173');
+    
       //Act
       cy.get('[data-cy="name-input"]').type("ekin");
       cy.get('[data-cy="lastname-input"]').type("eraydin");
@@ -71,15 +66,14 @@ describe('Register Page', () => {
 
     });
     it('Id is visible when all inputs are validated and button is clicked ', () => {
-      //Arrange
-      //cy.visit('http://localhost:5173');
-      //Act
+ 
+        //Act
       cy.get('[data-cy="name-input"]').type("ekin");
       cy.get('[data-cy="lastname-input"]').type("eraydin");
       cy.get('[data-cy="email-input"]').type("uekineraydin@gmail.com");
       cy.get('[data-cy="password-input"]').type("1234AS!d");
       cy.get('[data-cy="submit-button"]').click();
-      //Assert
+   //Assert
      cy.get('[data-cy="id-output"]').should("be.visible");
 
     });
